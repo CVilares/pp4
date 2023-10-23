d3050f110bba1e06402aaa08013bab7dde148755
CVilares <>
2023-10-23 23:05:42 +0000

    bug fixed, deployment

--- flavor/settings.py
+++ flavor/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-cvilares-pp4-qebu75e7v3.us2.codeanyapp.com', 'flavor-blog-8ecb3df13f92.herokuapp.com, 'localhost']
+    '8000-cvilares-pp4-qebu75e7v3.us2.codeanyapp.com', 'flavor-blog-8ecb3df13f92.herokuapp.com', 'localhost']
 
 
 # Application definition
