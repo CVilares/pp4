135e4bbddaf4b828129d0645768370b60e1b66c8
CVilares <>
2023-10-24 13:41:03 +0000

    syntax error fixed, ready to migrate

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Post(models.model):
 """
 on_delete=models.CASCADE ,check later to keep this or not,
 """
-    title = models.Charfiels(max_length=250, unique=True)
+    title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250 unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
