1a48d6c5e912b151eb4470e4755184bb9f761077
CVilares <>
2023-10-24 13:38:30 +0000

    Added Comment model for handling post comments

--- blog/models.py
+++ blog/models.py
@@ -22,4 +22,41 @@ on_delete=models.CASCADE ,check later to keep this or not,
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
-    
+    class Meta:
+
+        """
+        Posts oder's by date descending
+        """
+        ordering = ['-created_on']
+
+    def __str__(self):
+        """
+        Returns a string representation of an object
+        """
+        return self.title
+
+    def number_of_likes(self):
+        """
+        returns number of likes
+        """
+        return self.likes.count()        
+
+    class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        """
+        The order of comments by date ascending order
+        """
+        ordering = ['created_on']
+
+    def __str__(self):
+        """
+        Returns comment with body text and name
+        """
+        returnf"Comment{self.body} by {self.name}"    
