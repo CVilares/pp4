ef2475f97f1a6d44c85556eb99e2add65a7c3fa6
CVilares <>
2023-10-24 23:17:17 +0000

    templates created

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,11 @@ from .models import Post
 
 
 class PostList(generic.ListView):
+
+    """
+    Displays the home page
+    """
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
     paginate_by = 6
