e7b3614cf25006e86edfd2357404d18d2e2b80a5
CVilares <>
2023-10-24 13:25:31 +0000

    Added model metadata and methods for enhanced functionality.

--- blog/models.py
+++ blog/models.py
@@ -8,14 +8,18 @@ class Post(models.model):
  """
  Database model for Posts including their title, content, author, and more.
  """
-
+"""
+on_delete=models.CASCADE ,check later to keep this or not,
+"""
     title = models.Charfiels(max_length=250, unique=True)
     slug = models.SlugField(max_length=250 unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('image', default='colocar imagem aqui')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    
