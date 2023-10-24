6df18355588eedd4559c03cc92d8e78af84f9810
CVilares <>
2023-10-24 13:15:56 +0000

    add database model for posts including title,contentt, author etc in models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Post(models.model):
+ """
+ Database model for Posts including their title, content, author, and more.
+ """
+
+    title = models.Charfiels(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250 unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
