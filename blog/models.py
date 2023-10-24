aadfb7822152b330bbf163750271d1411645b119
CVilares <>
2023-10-24 23:44:06 +0000

    runserver test

--- blog/models.py
+++ blog/models.py
@@ -4,21 +4,22 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
 class Post(models.Model):
     """
     Database model for Posts including their title, content, author, and more
     """
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    featured_image = CloudinaryField('image', default='colocar imagem aqui')
+    featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
-
 
     class Meta:
 
@@ -37,13 +38,15 @@ class Post(models.Model):
         """
         returns number of likes
         """
-        return self.likes.count()        
+        return self.likes.count()
+
 
 class Comment(models.Model):
     """
     Database model for Comment including post, email,name and more.
     """
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
@@ -60,4 +63,4 @@ class Comment(models.Model):
         """
         Returns comment with body text and name
         """
-        return 'Comment {} by {}'.format(self.body, self.name)    
+        return 'Comment {} by {}'.format(self.body, self.name)
