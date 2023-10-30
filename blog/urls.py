2bb058e3d084f31f1f4d58daf0948b2f7ce1b3b2
CVilares <>
2023-10-30 18:22:40 +0000

    adding some css

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 
 
 urlpatterns = [
-    
-    path('intro.html/', views.IntroView.as_view(), name='intro'),
-    path('', views.PostList.as_view(), name='home'),
+    # Set IntroView as the root URL view
+    path('', views.IntroView.as_view(), name='intro'),
+    # Optional, in case you want to support '/intro/'
+    # The home view, if needed
+    path('home/', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
