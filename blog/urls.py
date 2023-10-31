88a8373d697051e6aa1863d78fb8288f488c6be3
CVilares <>
2023-10-31 10:17:01 +0000

    uninstall psycopg2 and install

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 urlpatterns = [
     # Set IntroView as the root URL view
     path('', views.IntroView.as_view(), name='intro'),
-    # Optional, in case you want to support '/intro/'
+    
     # The home view, if needed
     path('home/', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
